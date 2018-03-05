ecddaf6aa946ec5bac9a6f001d55ded9a189382d
Daniel Bradley <banddstudios@gmail.com>
2018-03-05 16:05:55 -0500

    Fixed 600 AD Independent China
    Fixed 600 AD Tibetan Barbarians

--- Assets/Python/Barbs.py
+++ Assets/Python/Barbs.py
@@ -134,7 +134,9 @@ class Barbs:
 		#tibet
 		if utils.isYearIn(-350, 200):
 			self.checkSpawn(iBarbarian, iLightSwordsman, 1 + iHandicap, (92, 41), (99, 45), self.spawnMinors, iGameTurn, 10-iHandicap, 3, ["TXT_KEY_ADJECTIVE_TIBETAN"])
-		elif utils.isYearIn(200, 1100):
+		elif utils.isYearIn(200, 600):
+			self.checkSpawn(iBarbarian, iSwordsman, 1 + iHandicap, (92, 41), (99, 45), self.spawnMinors, iGameTurn, 10-iHandicap, 3, ["TXT_KEY_ADJECTIVE_TIBETAN"])
+		elif utils.isYearIn(660, 1100):
 			self.checkSpawn(iBarbarian, iSwordsman, 1 + iHandicap, (92, 41), (99, 45), self.spawnMinors, iGameTurn, 10-iHandicap, 3, ["TXT_KEY_ADJECTIVE_TIBETAN"])
 
 		# Deccan barbarians
