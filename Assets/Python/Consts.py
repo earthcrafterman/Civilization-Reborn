9f7f84a3bfa32a26f2f878e35671e444c2c1d73c
Daniel Bradley <banddstudios@gmail.com>
2018-03-29 13:39:48 -0400

    Tweaked Persia

--- Assets/Python/Consts.py
+++ Assets/Python/Consts.py
@@ -1157,7 +1157,7 @@ lEnemyCivsOnSpawn = [
 [], #India
 [iCeltia, iCeltia, iBabylonia, iBabylonia, iEgypt, iEgypt], #Carthage
 [], #Polynesia
-[iBabylonia,iBabylonia,iGreece,iCarthage,iCarthage,iEgypt,iEgypt,iIndependent2], #Persia
+[iBabylonia,iBabylonia,iIndependent2,iIndependent2,iGreece,iCarthage,iEgypt], #Persia
 #[iEgypt,iGreece,iGreece,iCarthage,iCarthage], #Rome
 [iCeltia], # rome for testing
 [], #Tamils
